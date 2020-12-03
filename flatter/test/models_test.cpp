0cb12a22ae0094fead8e7b93748d55200349fa3f
Joaquin <joaquin.f.fernandez@gmail.com>
2020-12-03 11:39:09 -0300

    [iss-22] Squashed commit of the following:
    commit 1ce11612605fb2bf129600f23ffb952e08b781f5
    Author: Joaquin <joaquin.f.fernandez@gmail.com>
    Date:   Thu Dec 3 11:06:09 2020 -0300
    
        Fixed gtest install rule.
    
    commit b7c1bc5c9be38c5730f281c0406230a31f3b1c46
    Author: Joaquin <joaquin.f.fernandez@gmail.com>
    Date:   Wed Dec 2 20:38:02 2020 -0300
    
        Added test Makefile.
    
    commit 4c552eecb0f0cbfb16f0f1f9770641bc4bf94ac9
    Author: Joaquin <joaquin.f.fernandez@gmail.com>
    Date:   Wed Dec 2 20:37:14 2020 -0300
    
        Don't build gtest by fefault.
    
    commit f4b1fcddadea4ec323984c9cc5ac0a074562d9e4
    Merge: 1761f03 2a64004
    Author: Joaquin <joaquin.f.fernandez@gmail.com>
    Date:   Wed Dec 2 19:53:50 2020 -0300
    
        Merge branch 'modelicacc-dev' into iss-22-add-itests-for-flatter
    
    commit 1761f036dd948180e435db0931a62d9cf09cc9c7
    Merge: 9777753 b6aaa47
    Author: Joaquin <joaquin.f.fernandez@gmail.com>
    Date:   Wed Dec 2 19:47:37 2020 -0300
    
        Merge branch 'modelicacc-dev' into iss-22-add-itests-for-flatter
    
    commit 9777753faf500ea2e92529e62d029fd2908dddbc
    Author: Joaquin <joaquin.f.fernandez@gmail.com>
    Date:   Mon Nov 30 18:29:10 2020 -0300
    
        Added test action to workflow.
    
    commit 1850a770f9fcf5af89c7916972c2bc5e690ae96a
    Author: Joaquin <joaquin.f.fernandez@gmail.com>
    Date:   Mon Nov 30 18:28:51 2020 -0300
    
        Include test target in main makefile.
    
    commit 3542e5453aa0dbd16ae63a1a3e886a8031173bec
    Author: Joaquin <joaquin.f.fernandez@gmail.com>
    Date:   Mon Nov 30 18:28:28 2020 -0300
    
        Modifed flatted makefile, include test target.
    
    commit f0baf312378a1d911fbf5c67dc9b13823addbca7
    Author: Joaquin <joaquin.f.fernandez@gmail.com>
    Date:   Mon Nov 30 18:27:48 2020 -0300
    
        Added ITest files.
    
    commit a734156d04792367b38a2128cb2ae482ec9b8683
    Author: Joaquin <joaquin.f.fernandez@gmail.com>
    Date:   Mon Nov 30 18:27:22 2020 -0300
    
        Updated pre-commit hooks.
    
    commit 94adefeabacfcd0182426be34dbc8640d96fa368
    Author: Joaquin <joaquin.f.fernandez@gmail.com>
    Date:   Mon Nov 30 18:27:06 2020 -0300
    
        Updated .gitignore file.

---
+++ flatter/test/models_test.cpp
@@ -0,0 +1,55 @@
+/*****************************************************************************
+
+    This file is part of Modelica C Compiler.
+
+    Modelica C Compiler is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Modelica C Compiler is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Modelica C Compiler.  If not, see <http://www.gnu.org/licenses/>.
+
+******************************************************************************/
+
+#include <gtest/gtest.h>
+
+#include <cstdlib>
+#include <fstream>
+
+/// @file models_test.cpp
+///
+
+/// @addtogroup MCCITests
+/// @{
+
+class ITests : public testing::TestWithParam<const char*> {
+};
+
+TEST_P(ITests, GenerateCode)
+{
+  const std::string NAME = GetParam();
+  std::cout << "Testing model: " << NAME << std::endl;
+  const std::string FLATTER = "../../bin/flatter ";
+  const std::string ARGS = "../../test/mccprograms/" + NAME + ".mo";
+  const std::string RESULT_FILE = "./test_data/" + NAME + ".passed";
+  const std::string TEST_CMD = "./test_results.sh " + NAME;
+  const std::string FLATTER_CMD = FLATTER + ARGS + " > ./test_data/" + NAME + ".mo";
+
+  std::system(FLATTER_CMD.c_str());
+  std::system(TEST_CMD.c_str());
+
+  std::ifstream result(RESULT_FILE.c_str());
+  EXPECT_TRUE(result.good());
+}
+
+const char* models[] = {"SBGraph1", "SBGraph2", "SBGraph3", "SBGraph4"};
+
+INSTANTIATE_TEST_SUITE_P(Models, ITests, testing::ValuesIn(models));
+
+/// @}
