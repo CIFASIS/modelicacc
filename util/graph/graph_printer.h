f5fb536001d60fa0904199b9c6797ca432f7b4cf
Joaquin <joaquin.f.fernandez@gmail.com>
2021-03-12 17:56:38 -0300

    Split graph printer implementation.

--- util/graph/graph_printer.h
+++ util/graph/graph_printer.h
@@ -19,133 +19,27 @@
 #ifndef GRAPH_PRINTER_
 #define GRAPH_PRINTER_
 
-#include <list>
-#include <iostream>
-#include <fstream>
-#include <sstream>
-
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/lexical_cast.hpp>
-
 #include<util/graph/graph_definition.h>
 
 using namespace std;
 
-#define MAKE_SPACE for(int __i=0; __i<depth; __i++) stri << " ";
-#define TAB_SPACE 2
-#define INSERT_TAB depth += TAB_SPACE;
-#define DELETE_TAB depth -= TAB_SPACE;
-
 namespace SBG {
   class GraphPrinter{
     public:
-    GraphPrinter(const SBGraph &g, const int mod) 
-      : graph(g), mode(mod), depth(0){};
+    GraphPrinter(const SBGraph &g, const int mod);
 
-    void printGraph(std::string name){
-      stringstream stri;
-      ofstream out(name.c_str());
-      
-      stri << "digraph G{" << endl;
-      INSERT_TAB
-      MAKE_SPACE
-      stri << "  ratio=\"fill\"" << endl;
-      MAKE_SPACE
-      stri << "  node[shape=\"ellipse\"]" << endl;
-      INSERT_TAB
-      MAKE_SPACE
- 
-      // Vertices printing
-      printVertices(stri);
-      stri << "\n";
-  
-      DELETE_TAB
-      DELETE_TAB
-      
-      INSERT_TAB
-      INSERT_TAB
-      stringstream colors;
-
-      DELETE_TAB
-      stri << colors.str();
-      DELETE_TAB
-      
-      INSERT_TAB
-      INSERT_TAB
-
-      //Edge printing
-      printEdges(stri);
-
-      DELETE_TAB
-      DELETE_TAB
-      stri << "\n";
-      stri << "}" << endl;
-      out << stri.str();
-      out.close();
-    };
+    void printGraph(std::string name);
 
     private:
     int depth;
     const SBGraph &graph; 
     const int mode;
 
-    void printVertices(stringstream &stri){
-      switch(mode){
-        case 1: // Flatter
-          break;
-        case 2: // SetBased
-          break;
-        case 3: // Compacted
-          break;
-        default:
-          VertexIt vi, vi_end;
-          for(boost::tie(vi, vi_end) = boost::vertices(graph); vi != vi_end; ++vi){
-            stri << vPrinter(graph[*vi]) << " [label=\"" << vLabelPrinter(graph[*vi]) << "\"]";
-            stri << "\n";
-            MAKE_SPACE
-          }
-      }
-    };
-
-    void printEdges(stringstream &stri){
-      switch(mode){
-        case 1: // Flatter
-          break;
-        case 2: // SetBased
-          break;
-        case 3: // Compacted
-          break;
-        default:
-          EdgeIt ei, ei_end;
-          for(boost::tie(ei, ei_end) = boost::edges(graph); ei != ei_end; ++ei){
-            SetVertexDesc v1 = boost::source(*ei, graph);
-            SetVertexDesc v2 = boost::target(*ei, graph);
-            stri << vPrinter(graph[v1]) << " -> " << vPrinter(graph[v2]); 
-            stri << " [label=\"" << ePrinter(graph[*ei]) << "\", arrowhead=\"none\"]"; 
-            stri << "\n";
-            MAKE_SPACE
-          }
-      }
-    };
-
-    std::string vPrinter(SetVertex v){
-      return v.name;
-    };
-
-    std::string vLabelPrinter(SetVertex v){      
-      std::stringstream label;
-      SBG::Set dom = v.vs_();
-      label << "{ " << v.name << " Dom: " << dom << " }"; 
-      return label.str();
-    };
-
-    std::string ePrinter(SetEdge e){
-      std::stringstream label;
-      PWLMap mapF = e.es1_();
-      PWLMap mapU = e.es2_();
-      label << "{ " << e.name << " mapF: " << mapF << " mapU: " << mapU << " }";
-      return label.str();
-    };
+    void printVertices(stringstream &stri);
+    void printEdges(stringstream &stri);
+    std::string vPrinter(SetVertex v);
+    std::string vLabelPrinter(SetVertex v);
+    std::string ePrinter(SetEdge e);
   };
 } // namespace SBG
 
