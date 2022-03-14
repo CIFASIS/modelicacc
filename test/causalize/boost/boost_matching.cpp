6086577513d8bc4fc239a8859d19ba4946acd973
Joaquin <joaquin.f.fernandez@gmail.com>
2022-03-14 15:32:39 -0300

    [iss-61] Squashed commit of the following:
    commit e82fdfd84980d3be79fcd7b0ebbb39ee76a1bd70
    Author: Joaquin <joaquin.f.fernandez@gmail.com>
    Date:   Mon Mar 14 14:55:09 2022 -0300
    
        Setup logger in boost-cmp bijnary.
    
    commit 4a28dd5167dd999718222b60bbd21cfa76352ad9
    Author: Joaquin <joaquin.f.fernandez@gmail.com>
    Date:   Wed Mar 9 11:31:19 2022 -0300
    
        Update version and usage  messages for boost-cmp.
    
    commit 8356041fb990988b7f95b609316d4bb6b57813cb
    Author: Joaquin <joaquin.f.fernandez@gmail.com>
    Date:   Wed Mar 9 11:18:33 2022 -0300
    
        Added boost comparison binary for matching.
    
    commit c6f2a7b8559348498345e5858ede604ff3890274
    Author: Joaquin <joaquin.f.fernandez@gmail.com>
    Date:   Wed Mar 9 11:17:55 2022 -0300
    
        Update SBG library.
    
    commit 3390a47061621d4dacdd67646171bdd737d065e8
    Author: Joaquin <joaquin.f.fernandez@gmail.com>
    Date:   Wed Mar 9 11:17:37 2022 -0300
    
        Update stest object rule generation in Makefile.
    
    commit 5ad87b8e65a25dfe57b541175cc22e683b004ed1
    Author: Joaquin <joaquin.f.fernandez@gmail.com>
    Date:   Wed Mar 9 11:16:57 2022 -0300
    
        Use Logger in matching graph builder.
    
    commit fb5dddcfebc4c984f380a5d217d8caacfe7cac99
    Author: Joaquin <joaquin.f.fernandez@gmail.com>
    Date:   Wed Feb 23 14:51:11 2022 -0300
    
        Updated header files.
    
    commit 37250130bf6bf45015e7c6a30976200f836004b1
    Author: Joaquin <joaquin.f.fernandez@gmail.com>
    Date:   Wed Feb 23 14:50:56 2022 -0300
    
        Updated makefiles.
    
    commit d5b826043c160d08d8ec5527cb99126fe90736a9
    Author: Joaquin <joaquin.f.fernandez@gmail.com>
    Date:   Wed Feb 23 14:50:36 2022 -0300
    
        Added SBG library as 3rd party lib.
    
    commit 0255d7c6a17a4e003f8ad45a988e26edbbb8512a
    Author: Joaquin <joaquin.f.fernandez@gmail.com>
    Date:   Wed Feb 23 14:49:53 2022 -0300
    
        Updated gitignore.
    
    commit 099d80db12c0495ca2f4f39ba21446e04c5c2977
    Author: Joaquin <joaquin.f.fernandez@gmail.com>
    Date:   Wed Feb 23 14:47:06 2022 -0300
    
        Removed graph library files.

---
+++ test/causalize/boost/boost_matching.cpp
@@ -0,0 +1,187 @@
+/*****************************************************************************
+
+    This file is part of Modelica C Compiler.
+
+    Modelica C Compiler is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Modelica C Compiler is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Modelica C Compiler.  If not, see <http://www.gnu.org/licenses/>.
+
+******************************************************************************/
+
+#include <chrono>
+#include <string>
+#include <iostream>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/max_cardinality_matching.hpp>
+#include <boost/variant/get.hpp>
+#include <cassert>
+#include <getopt.h>
+
+
+#include <causalize/sbg_implementation/matching_graph_builder.h>
+#include <mmo/mmo_class.h>
+#include <parser/parser.h>
+#include <util/ast_visitors/state_variables_finder.h>
+#include <util/debug.h>
+#include <sbg/sbg.hpp>
+#include <sbg/sbg_algorithms.hpp>
+#include <sbg/sbg_printer.hpp>
+#include <sbg/graph_builders/ordinary_graph_builder.hpp>
+#include <util/logger.h>
+
+using namespace boost;
+using namespace std;
+
+using namespace Modelica;
+using namespace Modelica::AST;
+using namespace Causalize;
+
+void usage()
+{
+  cout << "Usage: boost-cpm [options] file" << endl;
+  cout << "Computes matching for a given Modelica model file using SBG and boost matching algorithms and shows the execution time of both." << endl;
+  cout << endl;
+  cout << "-d, --debug      Print debug information about graph generation." << endl;
+  cout << "-h, --help       Display this information and exit" << endl;
+  cout << "-o <path>, --output <path> Sets the output path for the generated graph dot file." << endl;
+  cout << endl;
+  cout << "Modelica C Compiler home page: https://github.com/CIFASIS/modelicacc " << endl;
+}
+
+void version()
+{
+  cout << "Modelica C Compiler 2.0" << endl;
+  cout << "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>" << endl;
+  cout << "This is free software: you are free to change and redistribute it." << endl;
+  cout << "There is NO WARRANTY, to the extent permitted by law." << endl;
+}
+
+//===============================================================================
+// Based on the example code from Aaron Windsor: 
+//
+// https://www.boost.org/doc/libs/1_46_0/libs/graph/example/matching_example.cpp
+//
+//===============================================================================
+
+void computeMaxCardinalityMatching(SBG::SBGraph sb_graph, std::string dot_file, bool debug)
+{
+
+  OG::OrdinaryGraphBuilder ordinary_graph_builder(sb_graph);
+
+  OG::Graph graph = ordinary_graph_builder.build();
+
+  if (debug) {
+    OG::GraphPrinter printer(graph);
+    printer.printGraph(dot_file+"_boost.dot");
+  }
+
+  int n_vertices = num_vertices(graph);
+
+  std::vector<graph_traits<OG::Graph>::vertex_descriptor> mate(n_vertices);
+  auto begin = std::chrono::high_resolution_clock::now();
+  checked_edmonds_maximum_cardinality_matching(graph, &mate[0]);
+  auto end = std::chrono::high_resolution_clock::now();
+  auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+
+  if (debug) {
+    std::cout << std::endl << "Boost: Found a matching of size " << matching_size(graph, &mate[0]) << std::endl;
+
+    std::cout << "The matching is:" << std::endl;
+    
+    graph_traits<OG::Graph>::vertex_iterator vi, vi_end;
+    for(tie(vi,vi_end) = vertices(graph); vi != vi_end; ++vi)
+      if (mate[*vi] != graph_traits<OG::Graph>::null_vertex() && *vi < mate[*vi])
+        std::cout << "{" << *vi << ", " << mate[*vi] << "}" << std::endl;
+
+    std::cout << std::endl;
+  }
+
+  std::cout << "Boost Edmonds Maximum cardinality matching time: " << milliseconds.count() << " " << std::endl;
+}
+
+int main(int argc, char **argv)
+{
+  int opt;
+  extern char* optarg;
+  string output_path = "";
+  bool debug = false;
+
+  while (true) {
+    static struct option long_options[] = {{"version", no_argument, 0, 'v'},
+                                           {"help", no_argument, 0, 'h'},
+                                           {"debug", no_argument, 0, 'd'},
+                                           {"output", required_argument, 0, 'o'},
+                                           {0, 0, 0, 0}};
+    int option_index = 0;
+    opt = getopt_long(argc, argv, "vhdo:", long_options, &option_index);
+    if (opt == EOF) {
+      break;
+    }
+    switch (opt) {
+    case 'v':
+      version();
+      exit(0);
+    case 'h':
+      usage();
+      exit(0);
+    case 'd':
+      debug = true;
+      break;
+    case 'o':
+      output_path = optarg;
+      break;
+    default:
+      abort();
+    }
+  }
+
+  StoredDef stored_def;
+  bool status = false;
+  if (argv[optind] != nullptr) {
+    stored_def = Parser::ParseFile(argv[optind], status);
+  } 
+
+  if (!status) {
+    return -1;
+  }
+
+  Class ast_c = boost::get<Class>(stored_def.classes().front());
+  MMO_Class mmo_class(ast_c);
+  StateVariablesFinder setup_state_var(mmo_class);
+  setup_state_var.findStateVariables();
+
+  std::string dot_file = output_path+mmo_class.name();
+  Logger::instance().setFile(dot_file);
+
+  MatchingGraphBuilder matching_graph_builder(mmo_class);
+
+  SBG::SBGraph matching_graph = matching_graph_builder.makeGraph();
+  SBG::GraphPrinter printer(matching_graph, 0);
+
+  printer.printGraph(dot_file+".dot");
+
+  MatchingStruct match(matching_graph);
+  auto begin = std::chrono::high_resolution_clock::now();
+  pair<SBG::Set, bool> res = match.SBGMatching();
+  auto end = std::chrono::high_resolution_clock::now();
+  auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+
+  cout << "Generated matching:\n";
+  cout << get<0>(res) << "\n\n";
+  if (get<1>(res))
+     cout << ">>> Matched all unknowns\n";
+  std::cout << "SBG Algorithn matching time: " << milliseconds.count() << std::endl;
+
+  computeMaxCardinalityMatching(matching_graph, dot_file, debug);
+
+  return 0;
+}
